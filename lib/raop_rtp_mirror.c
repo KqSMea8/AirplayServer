594ccb5ee766c8eb8822b6c1c3fd2f2ea59e2c32
FD_ <fdraschbacher@gmail.com>
2019-04-23 09:29:20 +0200

    Temporarily added benchmarking to h264 decryption

--- lib/raop_rtp_mirror.c
+++ lib/raop_rtp_mirror.c
@@ -324,6 +324,17 @@ raop_rtp_mirror_thread(void *arg)
                     fwrite(payload_in, payloadsize, 1, file_source);
                     fwrite(&readstart, sizeof(readstart), 1, file_len);
 #endif
+      
+                    struct timeval tval_before, tval_after, tval_result;
+                    gettimeofday(&tval_before, NULL);
+
+                    // Decrypt data
+                    mirror_buffer_decrypt(raop_rtp_mirror->buffer, payload_in, payload, payloadsize);
+
+                    gettimeofday(&tval_after, NULL);
+                    timersub(&tval_after, &tval_before, &tval_result);
+                    printf("H264 decryption took: %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+
                     // Decrypt data
                     mirror_buffer_decrypt(raop_rtp_mirror->buffer, payload_in, payload, payloadsize);
                     int nalu_size = 0;
