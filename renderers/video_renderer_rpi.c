77f3d9781cacdc50131489980d42a9f256bbfe2d
FD_ <fdraschbacher@gmail.com>
2019-09-03 11:20:49 +0200

    Fixed issue #14

--- renderers/video_renderer_rpi.c
+++ renderers/video_renderer_rpi.c
@@ -38,6 +38,9 @@
 */
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define LAYER_VIDEO_VISIBLE 2
+#define LAYER_BACKGROUND 1
+#define LAYER_VIDEO_INVISIBLE 0
 
 struct video_renderer_s {
     logger_t *logger;
@@ -67,7 +70,6 @@ void video_renderer_draw_background(){
     uint32_t vc_image_ptr;
     VC_IMAGE_TYPE_T type = VC_IMAGE_RGB565;
     uint16_t image = 0x0000; // black
-    int layer = 0;
 
     VC_RECT_T dst_rect, src_rect;
 
@@ -84,10 +86,26 @@ void video_renderer_draw_background(){
 
     update = vc_dispmanx_update_start(0);
 
-    vc_dispmanx_element_add(update, display, layer, &dst_rect, resource, &src_rect,
+    vc_dispmanx_element_add(update, display, LAYER_BACKGROUND, &dst_rect, resource, &src_rect,
         DISPMANX_PROTECTION_NONE, NULL, NULL, DISPMANX_STEREOSCOPIC_MONO);
 
     vc_dispmanx_update_submit_sync(update);
+}
+
+void video_renderer_set_video_layer(video_renderer_t *renderer, int layer) {
+    OMX_CONFIG_DISPLAYREGIONTYPE display_region;
+    memset(&display_region, 0, sizeof(OMX_CONFIG_DISPLAYREGIONTYPE));
+    display_region.nSize = sizeof(OMX_CONFIG_DISPLAYREGIONTYPE);
+    display_region.nVersion.nVersion = OMX_VERSION;
+    display_region.nPortIndex = 90;
+    display_region.set = OMX_DISPLAY_SET_LAYER;
+    display_region.fullscreen = OMX_TRUE;
+    display_region.layer = layer;
+
+    if (OMX_SetConfig(ilclient_get_handle(renderer->video_renderer), OMX_IndexConfigDisplayRegion,
+                      &display_region) != OMX_ErrorNone) {
+        logger_log(renderer->logger, LOGGER_ERR, "Could not update video layer!");
+    }
 }
 
 void video_renderer_destroy_decoder(video_renderer_t *renderer) {
@@ -116,7 +134,7 @@ int video_renderer_init_decoder(video_renderer_t *renderer) {
 
     bcm_host_init();
 
-    if (renderer->background) video_renderer_draw_background();
+    if (renderer->background) video_renderer_draw_background(renderer);
 
     if ((renderer->client = ilclient_init()) == NULL) {
       return -3;
@@ -210,8 +228,9 @@ int video_renderer_init_decoder(video_renderer_t *renderer) {
     display_region.nSize = sizeof(OMX_CONFIG_DISPLAYREGIONTYPE);
     display_region.nVersion.nVersion = OMX_VERSION;
     display_region.nPortIndex = 90;
-    display_region.set = OMX_DISPLAY_SET_FULLSCREEN;
+    display_region.set = OMX_DISPLAY_SET_FULLSCREEN | OMX_DISPLAY_SET_LAYER;
     display_region.fullscreen = OMX_TRUE;
+    display_region.layer = LAYER_VIDEO_VISIBLE;
 
     if (OMX_SetConfig(ilclient_get_handle(renderer->video_renderer), OMX_IndexConfigDisplayRegion,
             &display_region) != OMX_ErrorNone) {
@@ -260,6 +279,7 @@ video_renderer_t *video_renderer_init(logger_t *logger, bool background, bool lo
 
     renderer->first_packet_time = 0;
     renderer->input_frames = 0;
+    renderer->hidden = false;
 
     if (video_renderer_init_decoder(renderer) != 1) {
         free(renderer);
@@ -357,6 +377,7 @@ void video_renderer_render_buffer(video_renderer_t *renderer, raop_ntp_t *ntp, u
             buffer->nFlags = OMX_BUFFERFLAG_STARTTIME;
             renderer->first_packet_time = raop_ntp_get_local_time(ntp);
             if (!renderer->low_latency) buffer->nTimeStamp = ilclient_ticks_from_s64(renderer->first_packet_time);
+            video_renderer_set_video_layer(renderer, LAYER_VIDEO_VISIBLE);
         }
 
         // Mark the last buffer if we had to split the data (probably not necessary)
@@ -381,7 +402,9 @@ void video_renderer_render_buffer(video_renderer_t *renderer, raop_ntp_t *ntp, u
 
 void video_renderer_flush(video_renderer_t *renderer) {
     ilclient_flush_tunnels(renderer->tunnels, 0);
-    if (renderer->background) video_renderer_draw_background();
+
+    video_renderer_set_video_layer(renderer, LAYER_VIDEO_INVISIBLE);
+    renderer->first_packet_time = 0;
 }
 
 void video_renderer_destroy(video_renderer_t *renderer) {
