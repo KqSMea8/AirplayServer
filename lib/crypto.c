b9e096354fce474d5c9ba1e4a04a85bb9f8cdb03
Derrick Lyndon Pallas <derrick@pallas.us>
2021-11-11 12:32:15 -0800

    lib/crypto: fix up OpenSSL AES calls
    Previously, we weren't calling Final after Update when decrypting (or
    encrypting) via AES.  My assumption is that this is because it was failing
    when due to a padding issue that caused Final to bail out.  This change
    turns off padding for both encrypt/decrypt and adds the call to final.
    
    We should probably consider having a static context that is reset after the
    call rather than allocating/deallocating a new CTX every time.  I'm going to
    punt on that for now though.
    
    Issue: FD-/RPiPlay#283

--- lib/crypto.c
+++ lib/crypto.c
@@ -68,25 +68,32 @@ aes_ctx_t *aes_init(const uint8_t *key, const uint8_t *iv, const EVP_CIPHER *typ
 
     memcpy(ctx->key, key, AES_128_BLOCK_SIZE);
     memcpy(ctx->iv, iv, AES_128_BLOCK_SIZE);
+    EVP_CIPHER_CTX_set_padding(ctx->cipher_ctx, 0);
     return ctx;
 }
 
 void aes_encrypt(aes_ctx_t *ctx, const uint8_t *in, uint8_t *out, int in_len) {
-    int out_len = 0;
-    if (!EVP_EncryptUpdate(ctx->cipher_ctx, out, &out_len, in, in_len)) {
+    int out_len_e = 0;
+    if (!EVP_EncryptUpdate(ctx->cipher_ctx, out, &out_len_e, in, in_len)) {
         handle_error(__func__);
     }
-
-    assert(out_len <= in_len);
+    int out_len_f = in_len - out_len_e;
+    if (!EVP_EncryptFinal_ex(ctx->cipher_ctx, out + out_len_e, &out_len_f)) {
+        handle_error(__func__);
+    }
+    assert(out_len_e + out_len_f <= in_len);
 }
 
 void aes_decrypt(aes_ctx_t *ctx, const uint8_t *in, uint8_t *out, int in_len) {
-    int out_len = 0;
-    if (!EVP_DecryptUpdate(ctx->cipher_ctx, out, &out_len, in, in_len)) {
+    int out_len_d = 0;
+    if (!EVP_DecryptUpdate(ctx->cipher_ctx, out, &out_len_d, in, in_len)) {
         handle_error(__func__);
     }
-
-    assert(out_len <= in_len);
+    int out_len_f = in_len - out_len_d;
+    if (!EVP_DecryptFinal_ex(ctx->cipher_ctx, out + out_len_d, &out_len_f)) {
+        handle_error(__func__);
+    }
+    assert(out_len_f + out_len_d <= in_len);
 }
 
 void aes_destroy(aes_ctx_t *ctx) {
