403e49eba9b221597bc84e5f4411fa8138b5bd5e
Luciano da Silva Ribas <lucribas@users>
2023-03-29 11:37:11 -0300

    tried set low-latency for enynos and fixed formating

--- app/src/main/java/com/fang/myapplication/MainActivity.java
+++ app/src/main/java/com/fang/myapplication/MainActivity.java
@@ -14,95 +14,92 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    public static String TAG = "AIS-RAOP-Main";
+	public static String TAG = "AIS-RAOP-Main";
 
-    private AirPlayServer mAirPlayServer;
-    private RaopServer mRaopServer;
-    private DNSNotify mDNSNotify;
+	private AirPlayServer mAirPlayServer;
+	private RaopServer mRaopServer;
+	private DNSNotify mDNSNotify;
 
-    private SurfaceView mSurfaceView;
-    private Button mBtnControl;
-    private TextView mTxtDevice;
-    private boolean mIsStart = false;
+	private SurfaceView mSurfaceView;
+	private Button mBtnControl;
+	private TextView mTxtDevice;
+	private boolean mIsStart = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSystemService(Context.NSD_SERVICE);
-        mBtnControl = findViewById(R.id.btn_control);
-        mTxtDevice = findViewById(R.id.txt_device);
-        mBtnControl.setOnClickListener(this);
-        mSurfaceView = findViewById(R.id.surface);
-        mAirPlayServer = new AirPlayServer();
-        mRaopServer = new RaopServer(mSurfaceView);
-        mDNSNotify = new DNSNotify();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getSystemService(Context.NSD_SERVICE);
+		mBtnControl = findViewById(R.id.btn_control);
+		mTxtDevice = findViewById(R.id.txt_device);
+		mBtnControl.setOnClickListener(this);
+		mSurfaceView = findViewById(R.id.surface);
+		mAirPlayServer = new AirPlayServer();
+		mRaopServer = new RaopServer(mSurfaceView);
+		mDNSNotify = new DNSNotify();
 
-        MediaCodecList mediaCodecList = new MediaCodecList(MediaCodecList.ALL_CODECS);
-        MediaCodecInfo[] mediaCodecInfos = mediaCodecList.getCodecInfos();
+		MediaCodecList mediaCodecList = new MediaCodecList(MediaCodecList.ALL_CODECS);
+		MediaCodecInfo[] mediaCodecInfos = mediaCodecList.getCodecInfos();
+		for (int i = 0; i < mediaCodecInfos.length; i++) {
+			if (mediaCodecInfos[i].isEncoder()) {
+				continue;
+			}
+			Log.d(TAG, "codec= " + mediaCodecInfos[i].getName() +
+					"\nis_encoder=" + mediaCodecInfos[i].isEncoder() +
+					"\nis_vendor=" + mediaCodecInfos[i].isVendor() +
+					"\nhw_acc=" + mediaCodecInfos[i].isHardwareAccelerated() +
+					"\nsw_acc=" + mediaCodecInfos[i].isSoftwareOnly());
+			String[] types = mediaCodecInfos[i].getSupportedTypes();
+			Log.d(TAG, "supported codec = " + String.join(", ", types));
 
-        for (int i = 0; i < mediaCodecInfos.length; i++) {
-            if (mediaCodecInfos[i].isEncoder()) {
-                continue;
-            }
-            Log.d(TAG, "codec= " + mediaCodecInfos[i].getName() +
-                    "\nis_encoder="+ mediaCodecInfos[i].isEncoder() +
-                    "\nis_vendor=" + mediaCodecInfos[i].isVendor() +
-                    "\nhw_acc=" + mediaCodecInfos[i].isHardwareAccelerated() +
-                    "\nsw_acc=" + mediaCodecInfos[i].isSoftwareOnly()
-                    );
-            String[] types = mediaCodecInfos[i].getSupportedTypes();
-            Log.d(TAG, "supported codec = " + String.join(", ", types));
+//			mediaCodecInfos[i].VideoCapabilities.getSupportedPerformancePoints();
+			// for (int j = 0; j < types.length; j++) {
+			// Log.d(TAG, "supported codec = " + types[j]);
+			// }
+		}
+	}
 
-//            for (int j = 0; j < types.length; j++) {
-//                Log.d(TAG, "supported codec = " + types[j]);
-//            }
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.btn_control: {
+				if (!mIsStart) {
+					startServer();
+					mTxtDevice.setText("Device name:" + mDNSNotify.getDeviceName());
+				} else {
+					stopServer();
+					mTxtDevice.setText("have not started");
+				}
+				mIsStart = !mIsStart;
+				mBtnControl.setText(mIsStart ? "End" : "Start");
+				break;
+			}
+		}
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_control: {
-                if (!mIsStart) {
-                    startServer();
-                    mTxtDevice.setText("Device name:" + mDNSNotify.getDeviceName());
-                } else {
-                    stopServer();
-                    mTxtDevice.setText("have not started");
-                }
-                mIsStart = !mIsStart;
-                mBtnControl.setText(mIsStart ? "End": "Start");
-                break;
-            }
-        }
-    }
+	private void startServer() {
+		mDNSNotify.changeDeviceName();
+		mAirPlayServer.startServer();
+		int airplayPort = mAirPlayServer.getPort();
+		if (airplayPort == 0) {
+			Toast.makeText(this.getApplicationContext(), "Start the AirPlay service failed", Toast.LENGTH_SHORT).show();
+		} else {
+			mDNSNotify.registerAirplay(airplayPort);
+		}
+		mRaopServer.startServer();
+		int raopPort = mRaopServer.getPort();
+		if (raopPort == 0) {
+			Toast.makeText(this.getApplicationContext(), "Start the RAOP service failed", Toast.LENGTH_SHORT).show();
+		} else {
+			mDNSNotify.registerRaop(raopPort);
+		}
+		Log.d(TAG, "airplayPort = " + airplayPort + ", raopPort = " + raopPort);
+	}
 
-    private void startServer() {
-        mDNSNotify.changeDeviceName();
-        mAirPlayServer.startServer();
-        int airplayPort = mAirPlayServer.getPort();
-        if (airplayPort == 0) {
-            Toast.makeText(this.getApplicationContext(),"Start the AirPlay service failed", Toast.LENGTH_SHORT).show();
-        } else {
-            mDNSNotify.registerAirplay(airplayPort);
-        }
-        mRaopServer.startServer();
-        int raopPort = mRaopServer.getPort();
-        if (raopPort == 0) {
-            Toast.makeText(this.getApplicationContext(), "Start the RAOP service failed",Toast.LENGTH_SHORT).show();
-        } else {
-            mDNSNotify.registerRaop(raopPort);
-        }
-        Log.d(TAG, "airplayPort = " + airplayPort + ", raopPort = " + raopPort);
-    }
-
-    private void stopServer() {
-        mDNSNotify.stop();
-        mAirPlayServer.stopServer();
-        mRaopServer.stopServer();
-    }
+	private void stopServer() {
+		mDNSNotify.stop();
+		mAirPlayServer.stopServer();
+		mRaopServer.stopServer();
+	}
 
 }
-
-
--- app/src/main/java/com/fang/myapplication/player/VideoPlayer.java
+++ app/src/main/java/com/fang/myapplication/player/VideoPlayer.java
@@ -1,7 +1,9 @@
 package com.fang.myapplication.player;
 
 import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
 import android.media.MediaFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Surface;
@@ -15,109 +17,143 @@ import java.util.List;
 
 public class VideoPlayer extends Thread {
 
-    private static final String TAG = "AIS-VideoPlayer";
+	private static final String TAG = "AIS-VideoPlayer";
 
-    private String mMimeType = "video/avc";
-    private int mVideoWidth  = 1920;
-    private int mVideoHeight = 1080;
-    private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
-    private MediaCodec mDecoder = null;
-    private Surface mSurface = null;
-    private boolean mIsEnd = false;
-    private List<NALPacket> mListBuffer = Collections.synchronizedList(new ArrayList<NALPacket>());
+	private String mMimeType = "video/avc";
+	private int mVideoWidth = 1280;
+	private int mVideoHeight = 720;
+	// private int mVideoWidth = 1920;
+	// private int mVideoHeight = 1080;
+	private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
+	private MediaCodec mDecoder = null;
+	private Surface mSurface = null;
+	private boolean mIsEnd = false;
+	private List<NALPacket> mListBuffer = Collections.synchronizedList(new ArrayList<NALPacket>());
 
-    public VideoPlayer(Surface surface) {
-        mSurface = surface;
-    }
+	public VideoPlayer(Surface surface) {
+		mSurface = surface;
+	}
 
-    public void initDecoder() {
-        try {
-            MediaFormat format = MediaFormat.createVideoFormat(mMimeType, mVideoWidth, mVideoHeight);
-            // working:
-            mDecoder = MediaCodec.createDecoderByType(mMimeType);
-//            mDecoder = MediaCodec.createByCodecName("OMX.MS.AVC.Decoder");
-//            mDecoder = MediaCodec.createByCodecName("OMX.google.h264.decoder");
-//            mDecoder = MediaCodec.createByCodecName("OMX.MS.VP8.Decoder");
 
-            //mDecoder = MediaCodec.createByCodecName("OMX.Exynos.avc.dec");
-            mDecoder.configure(format, mSurface, null, 0);
-            mDecoder.setVideoScalingMode(MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
-
-            Bundle lowLatency = new Bundle();
-            lowLatency.putInt(MediaCodec.PARAMETER_KEY_LOW_LATENCY , 1);
-            mDecoder.setParameters(lowLatency);
-
-            mDecoder.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void addPacker(NALPacket nalPacket) {
-        mListBuffer.add(nalPacket);
-    }
-    
-    @Override
-    public void run() {
-        super.run();
-        initDecoder();
-        while (!mIsEnd) {
-            if (mListBuffer.size() == 0) {
-                try {
-                    sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+    private static boolean decoderSupportsAndroidRLowLatency(MediaCodecInfo decoderInfo, String mimeType) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                if (decoderInfo.getCapabilitiesForType(mimeType).isFeatureSupported(MediaCodecInfo.CodecCapabilities.FEATURE_LowLatency)) {
+                    Log.d(TAG, "Low latency decoding mode supported (FEATURE_LowLatency)");
+                    return true;
                 }
-                continue;
+            } catch (Exception e) {
+                // Tolerate buggy codecs
+                e.printStackTrace();
             }
-            doDecode(mListBuffer.remove(0));
-        }
+        } else {
+			Log.e(TAG, "ERROR: Low latency decoding mode NOT supported (FEATURE_LowLatency)");
+		}
+
+        return false;
     }
 
-    private void doDecode(NALPacket nalPacket) {
-        final int TIMEOUT_USEC = 10000;
-        ByteBuffer[] decoderInputBuffers = mDecoder.getInputBuffers();
-        int inputBufIndex = -10000;
-        try {
-            inputBufIndex = mDecoder.dequeueInputBuffer(TIMEOUT_USEC);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (inputBufIndex >= 0) {
-            ByteBuffer inputBuf = decoderInputBuffers[inputBufIndex];
-            inputBuf.put(nalPacket.nalData);
-            mDecoder.queueInputBuffer(inputBufIndex, 0, nalPacket.nalData.length, nalPacket.pts, 0);
-        } else {
-            Log.d(TAG, "dequeueInputBuffer failed");
-        }
 
-        int outputBufferIndex = -10000;
-        try {
-            outputBufferIndex = mDecoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (outputBufferIndex >= 0) {
-            mDecoder.releaseOutputBuffer(outputBufferIndex, true);
-            try{
-                Thread.sleep(50);
-            }  catch (InterruptedException ie){
-                ie.printStackTrace();
-            }
-        } else if (outputBufferIndex == MediaCodec.INFO_TRY_AGAIN_LATER) {
-            try{
-                Thread.sleep(10);
-            }  catch (InterruptedException ie){
-                ie.printStackTrace();
-            }
-        } else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
-            // not important for us, since we're using Surface
+	public void initDecoder() {
+		try {
+			MediaFormat format = MediaFormat.createVideoFormat(mMimeType, mVideoWidth, mVideoHeight);
 
-        } else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
 
-        } else {
+			format.setInteger("low-latency", 1);
+			format.setInteger("vendor.rtc-ext-dec-low-latency.enable", 1);
 
-        }
 
-    }
+			// working:
+			mDecoder = MediaCodec.createDecoderByType(mMimeType);
+
+
+			MediaCodecInfo mDecoderInfo =  mDecoder.getCodecInfo();
+			Log.i(TAG, "DECODER SELECTED = " + mDecoderInfo.getName());
+			decoderSupportsAndroidRLowLatency(mDecoderInfo, format.getString(MediaFormat.KEY_MIME));
+
+			// mDecoder = MediaCodec.createByCodecName("OMX.MS.AVC.Decoder");
+			// mDecoder = MediaCodec.createByCodecName("OMX.google.h264.decoder");
+			// mDecoder = MediaCodec.createByCodecName("OMX.MS.VP8.Decoder");
+
+			// mDecoder = MediaCodec.createByCodecName("OMX.Exynos.avc.dec");
+			mDecoder.configure(format, mSurface, null, 0);
+			mDecoder.setVideoScalingMode(MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
+
+			// Bundle lowLatency = new Bundle();
+			// lowLatency.putInt(MediaCodec.PARAMETER_KEY_LOW_LATENCY, 1);
+			// mDecoder.setParameters(lowLatency);
+
+			mDecoder.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addPacker(NALPacket nalPacket) {
+		mListBuffer.add(nalPacket);
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		initDecoder();
+		while (!mIsEnd) {
+			if (mListBuffer.size() == 0) {
+				try {
+					sleep(50);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				continue;
+			}
+			doDecode(mListBuffer.remove(0));
+		}
+	}
+
+	private void doDecode(NALPacket nalPacket) {
+		final int TIMEOUT_USEC = 10000;
+		ByteBuffer[] decoderInputBuffers = mDecoder.getInputBuffers();
+		int inputBufIndex = -10000;
+		try {
+			inputBufIndex = mDecoder.dequeueInputBuffer(TIMEOUT_USEC);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (inputBufIndex >= 0) {
+			ByteBuffer inputBuf = decoderInputBuffers[inputBufIndex];
+			inputBuf.put(nalPacket.nalData);
+			mDecoder.queueInputBuffer(inputBufIndex, 0, nalPacket.nalData.length, nalPacket.pts, 0);
+		} else {
+			Log.d(TAG, "dequeueInputBuffer failed");
+		}
+
+		int outputBufferIndex = -10000;
+		try {
+			outputBufferIndex = mDecoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (outputBufferIndex >= 0) {
+			mDecoder.releaseOutputBuffer(outputBufferIndex, true);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		} else if (outputBufferIndex == MediaCodec.INFO_TRY_AGAIN_LATER) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		} else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+			// not important for us, since we're using Surface
+
+		} else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+
+		} else {
+
+		}
+
+	}
 }
